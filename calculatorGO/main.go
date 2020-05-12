2d2385b7a65c5ed504ecb6bae72434d32e250fed
rasna-skumar1306 <rasna5202@gmail.com>
2020-05-13 00:42:37 +0530

    main file.go --1

--- calculatorGO/main.go
+++ calculatorGO/main.go
@@ -5,13 +5,21 @@ import (
 
 	"github.com/go-init/calculatorGO/add"
 	"github.com/go-init/calculatorGO/diff"
+	"github.com/go-init/calculatorGO/sub"
 )
 
 func main() {
-	s := []int{1, 2, 3123, 124, 4, 5, 45, 56, 6, 67, 56, 3, 2}
-	f := []float64{1.5435, 345.76, 345345.7664, 345345.767}
+	s := []int{2, 3, 5, 7, 11}
+	f := []float64{2.1, 3.2, 5.3, 7.4, 8.5}
+	ff := []float32{2.1, 3.2, 5.3, 7.4, 8.5}
 	v := add.Addi(s...)
 	fv := add.Addf(f...)
 	sc := diff.Diffi(s...)
-	fmt.Println(v, fv, sc)
+	sf := diff.Difff32(ff...)
+	sf64 := diff.Difff64(f...)
+	si := sub.Subi(s...)
+	df := sub.Subf32(ff...)
+	df64 := sub.Subf(f...)
+	fmt.Println(v, fv, sc, sf, sf64, si, df, df64)
+	fmt.Printf("%T,%T,%T", sc, sf, sf64)
 }
