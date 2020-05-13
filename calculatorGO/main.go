d2ed5014b8207c5fb13aa50d20df23316425013f
rasna-skumar1306 <rasna5202@gmail.com>
2020-05-13 19:17:58 +0530

    main --2

--- calculatorGO/main.go
+++ calculatorGO/main.go
@@ -9,17 +9,21 @@ import (
 )
 
 func main() {
-	s := []int{2, 3, 5, 7, 11}
+	s := []int{}
 	f := []float64{2.1, 3.2, 5.3, 7.4, 8.5}
 	ff := []float32{2.1, 3.2, 5.3, 7.4, 8.5}
 	v := add.Addi(s...)
 	fv := add.Addf(f...)
-	sc := diff.Diffi(s...)
-	sf := diff.Difff32(ff...)
-	sf64 := diff.Difff64(f...)
+	sc, erri := diff.Diffi(s...)
+	sf, errf := diff.Difff32(ff...)
+	sf64, errff := diff.Difff64(f...)
 	si := sub.Subi(s...)
 	df := sub.Subf32(ff...)
 	df64 := sub.Subf(f...)
+	if (erri != nil) || (errff != nil) || (errf != nil) {
+		fmt.Println(erri, errf, errff)
+	}
 	fmt.Println(v, fv, sc, sf, sf64, si, df, df64)
 	fmt.Printf("%T,%T,%T", sc, sf, sf64)
+
 }
