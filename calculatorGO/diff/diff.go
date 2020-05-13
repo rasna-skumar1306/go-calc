3094d13bcfa8ee15444bd19ce1afd4f3daef3812
rasna-skumar1306 <rasna5202@gmail.com>
2020-05-13 19:17:16 +0530

    adding nil and unit handlers pkg diff

--- calculatorGO/diff/diff.go
+++ calculatorGO/diff/diff.go
@@ -2,11 +2,21 @@
 //two consecutive elements in []int, []float32 and []float64
 package diff
 
+import (
+	"errors"
+)
+
 //Diffi calculates the difference between each consecutive element of []int
 //and returns the value as []int of difference elements
-func Diffi(xi ...int) []int {
+func Diffi(xi ...int) ([]int, error) {
 	s := make([]int, 15)
 	j := 1
+	if len(xi) == 0 {
+		return []int{0}, errors.New("The array is empty expect atleast one element")
+	}
+	if len(xi) == 1 {
+		return xi, nil
+	}
 	for i := 0; i < len(xi); i++ {
 		s[i] = 0
 	}
@@ -17,13 +27,19 @@ func Diffi(xi ...int) []int {
 			break
 		}
 	}
-	return s[:len(xi)-1]
+	return s[:len(xi)-1], nil
 }
 
 //Difff32 calculates the difference between each consecutive element of []float32
 //and returns the value as []float32 of difference elements
-func Difff32(xi ...float32) []float32 {
+func Difff32(xi ...float32) ([]float32, error) {
 	s := make([]float32, 15)
+	if len(xi) == 0 {
+		return []float32{0}, errors.New("The array is empty expect atleast one element")
+	}
+	if len(xi) == 1 {
+		return xi, nil
+	}
 	j := 1
 	for i := 0; i < len(xi); i++ {
 		s[i] = 0
@@ -35,13 +51,19 @@ func Difff32(xi ...float32) []float32 {
 			break
 		}
 	}
-	return s[:len(xi)-1]
+	return s[:len(xi)-1], nil
 }
 
 //Difff64 calculates the difference between each consecutive element of []float64
 //and returns the value as []float64 of difference elements
-func Difff64(xi ...float64) []float64 {
+func Difff64(xi ...float64) ([]float64, error) {
 	s := make([]float64, 15)
+	if len(xi) == 0 {
+		return []float64{0}, errors.New("The array is empty expect atleast one element")
+	}
+	if len(xi) == 1 {
+		return xi, nil
+	}
 	j := 1
 	for i := 0; i < len(xi); i++ {
 		s[i] = 0
@@ -53,5 +75,5 @@ func Difff64(xi ...float64) []float64 {
 			break
 		}
 	}
-	return s[:len(xi)-1]
+	return s[:len(xi)-1], nil
 }
--- calculatorGO/diff/diff_test.go
+++ calculatorGO/diff/diff_test.go
@@ -7,34 +7,51 @@ import (
 
 func TestDiffi(t *testing.T) {
 	n := make([]int, 5)
-	v := []int{-1, -2, -2, -4}
-	n = Diffi([]int{2, 3, 5, 7, 11}...)
+	var er error
+	var v []int
+	v, er = ([]int{-1, -2, -2, -4}), nil
+	n, err := Diffi([]int{2, 3, 5, 7, 11}...)
+	for i, c := range n {
+		if c != v[i] {
+			t.Error("got", c, err, "expected", v[i], er)
+		}
+	}
+
+}
+
+func TestDifff32(t *testing.T) {
+	n := make([]float32, 5)
+	var v []float32
+	var er error
+	v, er = []float32{-1.1000001, -2.1000001, -2.1, -1.0999999}, nil
+	n, err := Difff32([]float32{2.1, 3.2, 5.3, 7.4, 8.5}...)
 	for i, c := range n {
 		if c != v[i] {
 			t.Error("got", c, "expected", v[i])
 		}
 	}
-}
-
-func TestDifff32(t *testing.T) {
-	n := make([]float32, 5)
-	v := []float32{-1.1000001, -2.1000001, -2.1, -1.0999999}
-	n = Difff32([]float32{2.1, 3.2, 5.3, 7.4, 8.5}...)
-	for i, c := range n {
-		if c != v[i] {
-			t.Error("got", c, "expected", v[i])
-		}
+	if er != err {
+		t.Error("got", err, "expected", er)
 	}
 }
 
 func TestDifff64(t *testing.T) {
 	n := make([]float64, 5)
-	v := []float64{-1.1, -2.0999999999999996, -2.1000000000000005, -1.0999999999999996}
-	n = Difff64([]float64{2.1, 3.2, 5.3, 7.4, 8.5}...)
+	var v []float64
+	var er error
+	v, er = []float64{-1.1, -2.0999999999999996, -2.1000000000000005, -1.0999999999999996}, nil
+
+	n, err := Difff64([]float64{2.1, 3.2, 5.3, 7.4, 8.5}...)
+	if err != nil {
+		return
+	}
 	for i, c := range n {
 		if c != v[i] {
 			t.Error("got", c, "expected", v[i])
 		}
+	}
+	if er != err {
+		t.Error("got", err, "expected", er)
 	}
 }
 
